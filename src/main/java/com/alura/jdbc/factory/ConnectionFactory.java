5f2547ab419a4fff9de148c6daf83de0957609a0
Christian <chriscar.sam@gmail.com>
2023-04-04 17:42:54 -0500

    Creando un pool de conexiones

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,15 +1,26 @@
 package com.alura.jdbc.factory;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
+	
+	private DataSource dataSource;
+	
+	public ConnectionFactory() {
+		var pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource.setJdbcUrl("jdbc:mysql://172.17.0.2/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSource.setUser("user");
+		pooledDataSource.setPassword("clave");
+		
+		this.dataSource = pooledDataSource;
+	}
 
 	public Connection recupetaConexion() throws SQLException {
-		return DriverManager.getConnection(
-	                "jdbc:mysql://172.17.0.2/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-	                "user",
-	                "clave");		 
+		return this.dataSource.getConnection();
 	}
 }
